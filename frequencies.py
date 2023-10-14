7216b70e89a237d0921ddde1d0e313bb74fa2840
Reema Alhamdan <36135736+reema7667@users.noreply.github.com>
2023-10-14 21:36:11 +0100

    add modified gitignore, add solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] +=1
+        else:
+            frequencies[item]=1
+            
+
     # Your code goes here
     return frequencies
